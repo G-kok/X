d4ddd31ec8fec10ec7ce460946596af1f0fcfef8
G-kok <135520574+G-kok@users.noreply.github.com>
2023-07-13 09:54:36 +0800

    Create bilibili.js

---
+++ Scripts/bilibili/bilibili.js
@@ -0,0 +1,11 @@
+  // 会员清晰度
+  if (obj.data.vip.status === 1) {
+    $done({});
+  } else {
+    obj.data.vip.type = 2;
+    obj.data.vip.status = 1;
+    obj.data.vip.vip_pay_type = 1;
+    obj.data.vip.due_date = 2208960000; // Unix 时间戳 2040-01-01 00:00:00
+    obj.data.vip.role = 3;
+  }
+} else if (url.includes("/x/v2/feed/index?")) {
